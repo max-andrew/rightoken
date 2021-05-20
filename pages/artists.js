757486bd4f194523fefd0ec4dc31ad7be5fefc5d
Max Andrew <maxsandrew@gmail.com>
2021-05-20 13:57:43 -0400

    Header

--- pages/artists.js
+++ pages/artists.js
@@ -55,131 +55,22 @@ export default function Home() {
 
 				<main>
 					<Header />
-					<Popover className="relative bg-white overflow-hidden">
-						{({ open }) => (
-							<>
-								<div className="max-w-7xl mx-auto">
-									<div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-										<svg
-											className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
-											fill="currentColor"
-											viewBox="0 0 100 100"
-											preserveAspectRatio="none"
-											aria-hidden="true"
-										>
-											<polygon points="50,0 100,0 50,100 0,100" />
-										</svg>
-
-										<div className="relative pt-6 px-4 sm:px-6 lg:px-8">
-											<nav
-												className="relative flex items-center justify-between sm:h-10 lg:justify-start"
-												aria-label="Global"
-											>
-												<div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
-													<div className="flex items-center justify-between w-full md:w-auto">
-														<a href="#">
-															<span className="sr-only">Workflow</span>
-															<img
-																className="h-8 w-auto sm:h-10"
-																src="/rightoken-logo.png"
-															/>
-														</a>
-														<div className="-mr-2 flex items-center md:hidden">
-															<Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-purple-500">
-																<span className="sr-only">Open main menu</span>
-																<MenuIcon className="h-6 w-6" aria-hidden="true" />
-															</Popover.Button>
-														</div>
-													</div>
-												</div>
-												<div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
-													{navigation.map((item) => (
-														<a key={item.name} href={item.href} className="font-medium text-gray-500 hover:text-gray-900">
-															{item.name}
-														</a>
-													))}
-													<a href="#" className="font-medium text-purple-600 hover:text-purple-500">
-														Beta test
-													</a>
-												</div>
-											</nav>
-										</div>
-
-										<Transition
-											show={open}
-											as={Fragment}
-											enter="duration-150 ease-out"
-											enterFrom="opacity-0 scale-95"
-											enterTo="opacity-100 scale-100"
-											leave="duration-100 ease-in"
-											leaveFrom="opacity-100 scale-100"
-											leaveTo="opacity-0 scale-95"
-										>
-											<Popover.Panel
-												focus
-												static
-												className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
-											>
-												<div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-													<div className="px-5 pt-4 flex items-center justify-between">
-														<div>
-															<img
-																className="h-8 w-auto"
-																src="/rightoken-logo.png"
-																alt=""
-															/>
-														</div>
-														<div className="-mr-2">
-															<Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-purple-500">
-																<span className="sr-only">Close main menu</span>
-																<XIcon className="h-6 w-6" aria-hidden="true" />
-															</Popover.Button>
-														</div>
-													</div>
-													<div className="px-2 pt-2 pb-3 space-y-1">
-														{navigation.map((item) => (
-															<a
-																key={item.name}
-																href={item.href}
-																className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
-															>
-																{item.name}
-															</a>
-														))}
-													</div>
-													<a
-														href="#"
-														className="block w-full px-5 py-3 text-center font-medium text-purple-600 bg-gray-50 hover:bg-gray-100"
-													>
-														Beta test
-													</a>
-												</div>
-											</Popover.Panel>
-										</Transition>
-
-										<main className="mt-8 w-full text-center flex items-center justify-center">
-											<div>
-												<img
-													className="h-24 w-auto"
-													src="/artists-hero.svg"
-													alt=""
-												/>
-											</div>
-										</main>
-									</div>
-								</div>
-							</>
-						)}
-					</Popover>
 
 					<div className="py-12 bg-white">
 						<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 							<div className="lg:text-center">
-								<h2 className="text-base text-purple-600 font-semibold tracking-wide uppercase">Tokenized Rights</h2>
+								<h2 className="text-base text-purple-600 font-semibold tracking-wide uppercase">Artists</h2>
 								<p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-									A key change in music
+									Your work. Your terms.
 								</p>
-								<p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+								<div className="mt-8 w-full text-center flex items-center justify-center">
+									<img
+										className="h-auto w-52"
+										src="/artists-hero.svg"
+										alt=""
+									/>
+								</div>
+								<p className="mt-6 max-w-2xl text-xl text-gray-500 lg:mx-auto">
 									By tokenizing rights, artists fund their work and keep control of the profits. Investors can share in the gains after finding the next hit.
 								</p>
 							</div>
