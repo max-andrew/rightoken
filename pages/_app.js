c2fddd3c6e19066e271b9d95f8372647de604478
Max Andrew <maxsandrew@gmail.com>
2021-06-15 21:43:51 -0400

    Working web3

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { Web3ReactProvider } from '@web3-react/core'
 import { Web3Provider } from '@ethersproject/providers'
 
 function getLibrary(provider) {
-	return new Web3Provider(provider) // this will vary according to whether you use e.g. ethers or web3.js
+	return new Web3Provider(provider)
 }
 
 function App({ Component, pageProps }) {
