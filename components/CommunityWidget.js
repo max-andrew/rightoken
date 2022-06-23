2beebbaff19032cbf4104c64b7531372e1ecf63f
Max Andrew <12118935+max-andrew@users.noreply.github.com>
2022-06-23 12:49:06 -0400

    Rearrange community buttons

--- components/CommunityWidget.js
+++ components/CommunityWidget.js
@@ -4,14 +4,14 @@ export default function CommunityWidget(props) {
 			<div className="flex flex-col justify-center items-center space-x-0 space-y-4 mt-10 sm:space-x-2 sm:space-y-0 sm:flex-row sm:space-x-8">
 				<div>
 					<a 
-						className="flex flex-col p-2 h-32 w-72 rounded-md shadow hover:shadow-lg active:shadow transition ease-in duration-200 ease-in-out overflow-hidden sm:h-48 sm:w-48 sm:p-4"
-						href="https://reddit.com/r/rightoken"
+						className="flex flex-col p-2 h-36 w-72 rounded-md shadow hover:shadow-lg active:shadow transition ease-in duration-200 ease-in-out overflow-hidden sm:h-48 sm:w-48 sm:p-4"
+						href="https://discord.gg/AZcKByZZ7j"
 						rel="_blank"
 					>
-						<p className="h-6 pt-2 pl-2 font-medium text-lg sm:pt-1 sm:pl-0 sm:text-xl">Join the community</p>
+						<p className="h-6 pt-2 pl-2 font-medium text-lg sm:pt-1 sm:pl-0 sm:text-xl">Talk directly</p>
 						<img
 							className="flex mt-6 ml-32 h-32 w-auto sm:ml-0 sm:mt-16 sm:h-48"
-							src="/reddit-logo.svg"
+							src="/discord-logo.svg"
 						/>
 					</a>
 				</div>
@@ -30,14 +30,14 @@ export default function CommunityWidget(props) {
 				</div>
 				<div>
 					<a 
-						className="flex flex-col p-2 h-36 w-72 rounded-md shadow hover:shadow-lg active:shadow transition ease-in duration-200 ease-in-out overflow-hidden sm:h-48 sm:w-48 sm:p-4"
-						href="https://discord.gg/AZcKByZZ7j"
+						className="flex flex-col p-2 h-32 w-72 rounded-md shadow hover:shadow-lg active:shadow transition ease-in duration-200 ease-in-out overflow-hidden sm:h-48 sm:w-48 sm:p-4"
+						href="https://reddit.com/r/rightoken"
 						rel="_blank"
 					>
-						<p className="h-6 pt-2 pl-2 font-medium text-lg sm:pt-1 sm:pl-0 sm:text-xl">Talk directly</p>
+						<p className="h-6 pt-2 pl-2 font-medium text-lg sm:pt-1 sm:pl-0 sm:text-xl">Join the community</p>
 						<img
 							className="flex mt-6 ml-32 h-32 w-auto sm:ml-0 sm:mt-16 sm:h-48"
-							src="/discord-logo.svg"
+							src="/reddit-logo.svg"
 						/>
 					</a>
 				</div>
