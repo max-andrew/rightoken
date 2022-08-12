1044df619c092d8bab92626d0a25a6e927615267
Max Andrew <12118935+max-andrew@users.noreply.github.com>
2022-08-12 00:53:11 -0400

    De-emphasize artist side
    Make the mint button in header smaller

--- components/Header.js
+++ components/Header.js
@@ -9,6 +9,19 @@ export default function Header(props) {
 		buttonText = "Mint your song"
 	}
 
+	let headerButton = <></>
+	if (linkTo === "support") {
+		headerButton = <p className="text-xs text-zinc-500 font-mono text-right underline mt-0.5"><a href="https://discord.gg/QCmetTcbPj" target="_blank" rel="noreferrer">Same-day help</a></p>
+	}
+	else if (linkTo === "artist") {
+		headerButton = <p className="text-xs text-zinc-500 font-mono text-right underline mt-0.5"><a href={"/"+linkTo} rel="noreferrer">I'm an artist</a></p>
+	}
+	else if (linkTo) {
+		headerButton = <form action={"/"+linkTo}>
+			<button className="uppercase text-xs md:text-sm text-zinc-400 font-bold border-r-2 border-b-2 border-l-2 border-zinc-400 rounded-b-md px-4 py-2 active:text-zinc-300 active:border-zinc-300">{buttonText}</button>
+		</form>
+	}
+
 	return (
 		<>
 			<div className="flex flex-row justify-between mx-auto z-40">
@@ -19,15 +32,7 @@ export default function Header(props) {
 						alt="rightoken logo"
 					/>
 				</a>
-				{ linkTo && linkTo !== "support" &&
-					<form action={"/"+linkTo}>
-						<button className="uppercase text-xs md:text-sm text-zinc-400 font-bold border-r-2 border-b-2 border-l-2 border-zinc-400 rounded-b-md px-4 py-2 active:text-zinc-300 active:border-zinc-300">{buttonText}</button>
-					</form>
-				}
-				{ linkTo === "support" &&
-					<p className="text-xs text-zinc-500 font-mono text-right underline mt-0.5"><a href="https://discord.gg/QCmetTcbPj" target="_blank" rel="noreferrer">Same-day help</a></p>
-					
-				}
+				{ headerButton }
 			</div>
 		</>
 	)
